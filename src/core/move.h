8ebc984d1bab7aadd0e9fad0c25651fddc2cd3f7
ashwin153 <ashwin.madavan@gmail.com>
2016-05-21 22:07:12 -0700

    [ADD] Move serialization
    Utilizes Boost to serialize moves to and from file. This allows moves to
    be saved to and from disk.

--- src/core/move.h
+++ src/core/move.h
@@ -3,6 +3,8 @@
 
 #include "position.h"
 #include <tuple>
+#include <functional>
+#include <type_traits>
 
 namespace chess {
 
@@ -11,7 +13,7 @@ namespace chess {
  * move type enumeration provides information to the game code as to how to
  * execute a particular move.
  */
-enum class MoveType {
+enum class MoveType : int {
 	kDefault,
 	kCastleKingside,
 	kCastleQueenside,
@@ -57,7 +59,49 @@ struct Move {
 	}	
 };
 
-}
+} // namespace chess
+
+namespace std {
+
+/*! Hash Move
+ * Defines how moves are hashed. Allows moves to be placed into unordered
+ * containers like unordered_map and unordered_set and allow for O(1) lookups.
+ */
+template <>
+struct hash<chess::Move> {
+private:
+	/*! Combine Hashes
+	 * Rather than having to link with Boost just to use the useful hash_combine
+	 * function, I just copy and pasted their implementation of this method.
+	 * Fucking sue me.
+	 * @param[in, out] seed Hash seed
+	 * @param[in] v Combine value
+	 */
+	template <class T>
+	inline void hash_combine(std::size_t& seed, const T& v) const {
+    	seed ^= hash<T>()(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+	}
+
+public:
+	/*! Hash
+	 * C++ probably has the most garbage way of implementing hashes for user
+	 * defined objects. Not only do you have to create this random struct in
+	 * namespace std (gross), but you also have to overload some random operator.
+	 * @param[in] move Move to hash
+	 * @return Hashed value
+	 */
+	size_t operator()(const chess::Move& move) const {
+		size_t seed = 0;
+		hash_combine(seed, hash<int>()(static_cast<int>(move.type)));
+		hash_combine(seed, hash<int>()(move.cur.x));
+		hash_combine(seed, hash<int>()(move.cur.y));
+		hash_combine(seed, hash<int>()(move.nxt.x));
+		hash_combine(seed, hash<int>()(move.nxt.y));
+		return seed;
+	}
+};
+
+} // namespace std
 
 #endif // CORE_MOVE_H
 
